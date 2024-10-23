31324039ca8f399adddd377edeed77405e5a39a6
kevinDupi <dupignyk@ufl.edu>
2024-10-23 16:58:33 -0400

    Added decode() function with a Global variable encoded_user_password to the main function

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ def encode(user_password):
         encoded_user_password = encoded_user_password + str(new_int)
     return encoded_user_password
 
+def decode(encoded_user_password): #decode function - 'Kevin'
+    passwordList = list(encoded_user_password)
+    for letter in range(len(passwordList)):
+        passwordList[letter] = str(int(passwordList[letter]) - 3)
 
-
+    decoded_password = "".join(passwordList)
+    return decoded_password
 
 
 
 #main function
 if __name__ == '__main__':
     #loop menu
+    encoded_user_password = '' #'Kevin' - added variable outside to be global for other function uses
     while True:
         print("Menu")
         print("-------------")
@@ -27,5 +33,7 @@ if __name__ == '__main__':
             user_password = input("Please enter your password to encode: ")
             encoded_user_password = encode(user_password)
             print("Your password has been encoded and stored!")
+        if user_input == 2: #Second option
+            print(f'The encoded password is {encoded_user_password}, and the original password is {decode(encoded_user_password)}')
         if user_input == 3:
             break
